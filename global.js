3294fcd9596074b343b6df0bf75d3963715789e0
Vishwaraj <74901922+Vishwaraj@users.noreply.github.com>
2022-09-21 18:03:58 +0530

    Update global.js

--- global.js
+++ global.js
@@ -1,2 +1,2 @@
 // export const API = 'http://localhost:3000/api/comments/replies';
-export const API = 'https://blog-comments-task-vishwaraj-442s3aglm-vishwarajk96-gmailcom.vercel.app/api/comments/replies'
+export const API = 'https://blog-comments-task-vishwaraj.vercel.app/api/comments/replies'
